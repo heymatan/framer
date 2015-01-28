8782151e03d34ea9df15e2bb0a64b11b4c1f4a0a
heymatan <heymatan@users.noreply.github.com>
2015-01-27 21:55:57 -0800

    Broke SCSS into more manageable chunks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt){
 		sass: {
 			dist: {
 				options: {
-					style: 'nested'
+					style: 'expanded'
 				},
 				files: {
 					'css/sass_compiled.css':'scss/styles.scss'
