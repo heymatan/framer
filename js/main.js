8f74aa40e137e7b3296c81b93205cee9e967d626
heymatan <heymatan@users.noreply.github.com>
2015-01-29 10:42:15 -0800

    Added slideshow

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.fill-selector li a').on('click',function(event){
+	$('.fill-selector li a').on('click.jpg',function(event){
 		event.preventDefault();
 		$('.fill-selector li').removeClass('active');
 		$(this).parent().addClass('active');
@@ -16,17 +16,40 @@ $(document).ready(function(){
 		$('#iframe-config').fadeIn();
 	});
 
-	$('.fill-selector #slideshow a').on('click',function(event){
-		event.preventDefault();
-		$('#iframe-config').fadeOut();
-		//add slideshows to frames
-	});
-
 	$('#iframe-address').on('submit',function(event){
 		var url = $('#iframe-config input[name=url]').val();
 		console.log(url);
 		//add iframes to frames
 		$('.device-frame .viewport').html('<iframe src="'+url+'"/>');
-		return false;
+		return false; //prevent GET
 	});
+	
+	$('.fill-selector #slideshow a').on('click',function(event){
+		event.preventDefault();
+		$('#iframe-config').fadeOut();
+		//add slideshows to frames
+		var images = [
+			'img/slideshow/mrscooks-site-home.jpg',
+			'img/slideshow/mrscooks-site-homehover.jpg',
+			'img/slideshow/mrscooks-site-about.jpg',
+			'img/slideshow/mrscooks-site-whatsnew.jpg',
+			'img/slideshow/mrscooks-site-shop.jpg',
+			'img/slideshow/mrscooks-site-product.jpg'
+		];
+		var slideshowMarkup = '<div class="flexslider">'
+		slideshowMarkup += '<ul class="slides">';
+		for (var i = 0; i < images.length; i++) {
+			slideshowMarkup += '<li>';
+			slideshowMarkup += '<img src="'+images[i]+'" />';
+			slideshowMarkup += '</li>';
+		};
+		slideshowMarkup += '</ul>';
+		slideshowMarkup += '</div>';
+		$('.device-frame .viewport').html(slideshowMarkup);
+		$('.flexslider').flexslider({
+			'controlNav':false
+		});
+	});
+
+	
 });
